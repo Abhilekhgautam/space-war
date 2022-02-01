7bdf6470c71fd62ff27a1679888b7c07e4db9a5d
abhilekhgautam <abhilekhgautam119@gmail.com>
2022-02-01 16:25:01 +0545

    Factory class for entity generation

--- src/EntityManager.cpp
+++ src/EntityManager.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "EntityManager.h"
+
+EntityManager :: EntityManager()
+               :m_totalEntities(0){}
+
+void EntityManager :: update(){
+    // update the entities here
+
+    for(auto& e : m_entitiesToAdd){
+        m_entities.push_back(e);
+    }
+}
+
+std::shared_ptr<Entity> EntityManager ::addEntity(std::string tag) {
+
+    auto entity = std::make_shared<Entity>(m_totalEntities++, tag);
+    m_entitiesToAdd.push_back(entity);
+    return  entity;
+
+}
+
+EntityVec EntityManager ::getEntities() {
+    return m_entities;
+}
+
+EntityVec EntityManager ::getEntities(std::string tag) {
+
+    //loop through the map and return the entities
+    return m_entities;
+
+}
--- src/EntityManager.h
+++ src/EntityManager.h
@@ -5,8 +5,31 @@
 #ifndef SPACE_WAR_ENTITYMANAGER_H
 #define SPACE_WAR_ENTITYMANAGER_H
 
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include "Entity.h"
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVec;
+typedef std::map<std::string, EntityVec> EntityMap;
+
 
 class EntityManager {
+
+    EntityVec m_entities;
+    EntityVec m_entitiesToAdd;
+
+    EntityMap m_entitiesMap;
+    size_t    m_totalEntities;
+
+    void removeDeadEntities(EntityVec& );
+
+public:
+    EntityManager();
+    void update();
+    std::shared_ptr<Entity> addEntity(std::string tag);
+    EntityVec getEntities();
+    EntityVec getEntities(std::string tag);
 
 };
 
